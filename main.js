549b95fddbefdce3c90b85d1fd5375566c41107a
yassershahofficial <geniususer4u@gmail.com>
2024-05-07 17:35:07 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+//pop up
+{
+    function togglePopup(){
+        let popup = document.getElementById("popup");
+        if(popup.style.visibility == "visible"){
+            popup.style.visibility = "hidden"
+        }
+        else{
+            popup.style.visibility = "visible"
+        }
+    }
+}
